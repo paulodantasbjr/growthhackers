1a2ddc56860be3cb707ba7e96de914db11708bc9
Paulo Dantas <paulo.dantas.solutic@petrobras.com.br>
2021-08-12 09:50:14 -0300

    fix bugs

--- client/components/Logo/index.jsx
+++ client/components/Logo/index.jsx
@@ -1,11 +1,17 @@
+import Link from 'next/link'
+
 import { LogoContainer } from './styled'
 
 export const Logo = () => {
   return (
     <LogoContainer>
-      <h2>
-        Growth<span>Shop</span>
-      </h2>
+      <Link href='/'>
+        <a>
+          <h2>
+            Growth<span>Shop</span>
+          </h2>
+        </a>
+      </Link>
     </LogoContainer>
   )
 }
--- client/pages/favorites/index.jsx
+++ client/pages/favorites/index.jsx
@@ -1,13 +1,38 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
+import Cookies from 'js-cookie'
+
 import { Layout } from '../../styles/Layout'
 import { FavoriteContainer } from './styled'
 import { FavoriteCard } from '../../components/FavoriteCard'
-
 import { DataContext } from '../../context/globalState'
+import { getData } from '../../utils/fetchData'
 
 export default function Favorites() {
-  const { state } = useContext(DataContext)
+  const { state, dispatch } = useContext(DataContext)
   const { auth } = state
+
+  useEffect(() => {
+    const token = Cookies.get('refreshtoken')
+    if (token) {
+      dispatch({ type: 'NOTIFY', payload: { loading: true } })
+      const fetchData = async () => {
+        const res = await getData('user/refresh_token', token)
+        dispatch({
+          type: 'AUTH',
+          payload: {
+            user: res.user,
+          },
+        })
+        dispatch({ type: 'NOTIFY', payload: {} })
+        if (res.err) {
+          localStorage.removeItem('firstLogin')
+          dispatch({ type: 'NOTIFY', payload: { error: err.msg } })
+          Cookies.remove('refreshtoken')
+        }
+      }
+      fetchData()
+    }
+  }, [])
 
   return (
     <Layout>
